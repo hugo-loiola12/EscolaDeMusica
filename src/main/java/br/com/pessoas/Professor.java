023062ede3e24390147803d266e10c97ea47a9c2
Hugo Loiola <hugoloiola.1@gmail.com>
2024-11-20 15:37:38 -0300

    Metodos do Professor

--- src/main/java/br/com/pessoas/Professor.java
+++ src/main/java/br/com/pessoas/Professor.java
@@ -1,15 +1,22 @@
 package br.com.pessoas;
 
-public class Funcionario extends Pessoa {
+public class Professor extends Pessoa {
 
     private final String funcao;
     private final double salario;
     private final String matricula;
 
-    public Funcionario(String nome, String cpf, String telefone, String email, String endereco, int idade, String funcao, double salario, String matricula) {
+    public Professor(String nome, String cpf, String telefone, String email, String endereco, int idade, String funcao, double salario, String matricula) {
         super(nome, cpf, telefone, email, endereco, idade);
         this.funcao = funcao;
         this.salario = salario;
         this.matricula = matricula;
     }
+
+    void agendarAula() {
+    }
+
+    void cancelarAula() {
+    }
+
 }
