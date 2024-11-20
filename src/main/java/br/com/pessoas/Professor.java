c131405e248af9fbccd1d86ffe41f705224376d8
Hugo Loiola <hugoloiola.1@gmail.com>
2024-11-20 15:53:11 -0300

    Interface e melhora nas classes

--- src/main/java/br/com/pessoas/Professor.java
+++ src/main/java/br/com/pessoas/Professor.java
@@ -1,22 +1,25 @@
 package br.com.pessoas;
 
-public class Professor extends Pessoa {
+import br.com.contrato.Agendamento;
+
+public class Professor extends Pessoa implements Agendamento {
 
     private final String funcao;
     private final double salario;
     private final String matricula;
 
-    public Professor(String nome, String cpf, String telefone, String email, String endereco, int idade, String funcao, double salario, String matricula) {
-        super(nome, cpf, telefone, email, endereco, idade);
+    public Professor(String nome, String cpf, int idade, String funcao, double salario, String matricula) {
+        super(nome, cpf, idade);
         this.funcao = funcao;
         this.salario = salario;
         this.matricula = matricula;
     }
 
-    void agendarAula() {
+    @Override
+    public void agendarAula() {
     }
 
-    void cancelarAula() {
+    @Override
+    public void cancelarAula() {
     }
-
 }
